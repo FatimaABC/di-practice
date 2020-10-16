fed42d3e88d5f6ffdc467a93caded9b864a566d5
Fatima <fatima.trtak@abc-techgroup.com>
2020-10-16 14:16:43 +0200

    property source example.

--- src/main/java/com/spring/di/DiApplication.java
+++ src/main/java/com/spring/di/DiApplication.java
@@ -1,6 +1,7 @@
 package com.spring.di;
 
 import com.spring.di.controller.*;
+import com.spring.di.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,31 +12,10 @@ public class DiApplication {
     public static void main(String[] args) {
 
         ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
-        PetController petController = (PetController) ctx.getBean("petController");
-        System.out.println(petController.whichPetIsTheBest());
-
-        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-        System.out.println(i18nController.sayGreeting());
-
         MyController myController = (MyController) ctx.getBean("myController");
-        System.out.println("----------------- Primary");
-        System.out.println(myController.sayHello());
 
-        System.out.println("----------------- property");
-
-        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-        System.out.println(propertyInjectedController.getGreeting());
-
-        System.out.println("----------------- setter");
-
-        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-        System.out.println(setterInjectedController.getGreeting());
-
-        System.out.println("------------------- constructor");
-
-        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-        System.out.println(constructorInjectedController.getGreeting());
-
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser());
 
     }
 
